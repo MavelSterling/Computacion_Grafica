feb77546773085fd3ae2071c04ee02bf9a7909cf
MavelSterling <mavelynsl@gmail.com>
2023-05-24 22:59:52 -0500

    Update manifolds.py

--- Objetos/manifolds.py
+++ Objetos/manifolds.py
@@ -3,3 +3,63 @@
 # La representación de manifolds en Python con OpenGL implica 
 # generar y renderizar una malla poligonal que aproxime la forma del manifold. 
 
+# Puedes utilizar bibliotecas como NumPy para generar la geometría 
+# del manifold y luego utilizar PyOpenGL para renderizarlo. 
+
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+import numpy as np
+
+# Definir las coordenadas de los vértices del manifold
+vertices = np.array([
+    [0.0, 0.0, 0.0],  # Vértice 1
+    [1.0, 0.0, 0.0],  # Vértice 2
+    [0.5, 1.0, 0.0],  # Vértice 3
+    [0.0, 0.5, 0.0]   # Vértice 4
+])
+
+# Definir las conexiones entre los vértices (aristas o caras)
+edges = np.array([
+    [0, 1],  # Arista 1 (conecta vértice 1 y vértice 2)
+    [1, 2],  # Arista 2 (conecta vértice 2 y vértice 3)
+    [2, 0],  # Arista 3 (conecta vértice 3 y vértice 1)
+    [0, 3],  # Arista 4 (conecta vértice 1 y vértice 4)
+    [3, 2]   # Arista 5 (conecta vértice 4 y vértice 3)
+])
+
+def render():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+
+    glBegin(GL_LINES)
+    glColor3f(1.0, 0.0, 0.0)  # Establecer el color de las aristas a rojo
+    for edge in edges:
+        for vertex_index in edge:
+            glVertex3fv(vertices[vertex_index])
+    glEnd()
+
+    glutSwapBuffers()
+
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
+    glutInitWindowSize(800, 600)
+    glutCreateWindow("Manifold OpenGL")
+
+    glutDisplayFunc(render)
+    glutMainLoop()
+
+if __name__ == '__main__':
+    main()
+
+
+# En este ejemplo, hemos definido un conjunto de vértices y conexiones para representar 
+# un manifold básico. La función render se encarga de dibujar las aristas del manifold 
+# utilizando glBegin(GL_LINES) para indicar que estamos dibujando líneas. 
+# Luego, iteramos sobre las conexiones y los vértices correspondientes para generar 
+# las líneas que forman el manifold. Utilizamos glVertex3fv para especificar 
+# las coordenadas de los vértices en formato de array
+
+# En el bucle principal, establecemos el tamaño de la ventana, creamos la ventana y 
+# configuramos las funciones de renderizado. Luego, ejecutamos el bucle principal 
+# con glutMainLoop() para comenzar a renderizar el manifold.
